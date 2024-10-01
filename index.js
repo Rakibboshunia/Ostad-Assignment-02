f401a6e0aaff975f7865be2143615c7bdc9b29cf
MD. AL-RAKEB RASEL BOSHUNIA <122854701+Rakibboshunia@users.noreply.github.com>
2024-10-01 15:45:08 +0600

    index.js

--- index.js
+++ index.js
@@ -6,25 +6,27 @@ var multer = require('multer');
 
 //Create HTTP server
 var server=http.createServer(function (req,res) {
-
     if(req.url=="/") 
     {
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write('<h1>This is About Page</h1>')
         res.end();
     }
+        
     else if(req.url=="/about") 
     {
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write('<h1>This is About Page</h1>')
         res.end();
     }
+        
     else if(req.url=="/contact") 
     {
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write('<h1>This is Contact Page</h1>')
         res.end();
     }
+        
 // File Write
     else if(req.url="/")
     {
